e9b9cdde511589ef4806ea0477a1be6b2390df13
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2023-06-20 14:36:02 +0700

    Add Edit Data RT endpoint

--- apps/rt/controller.js
+++ apps/rt/controller.js
@@ -53,6 +53,70 @@ exports.createRT = async (req, res, next) => {
   }
 };
 
+exports.editRT = async (req, res, next) => {
+  try {
+    /* Creating validation */
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation error, entered data is incorrect");
+      error.statusCode = 422;
+      throw err;
+    }
+
+    /* Get data from request session */
+    const { email } = req.user;
+    const checkWarga = await Warga.findOne({ email: email });
+    if (!checkWarga) {
+      const error = new Error("Email tidak ditemukan");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    /* Check if the user is the ketuaRT */
+    const { tokenRT } = req.params;
+    const rt = await RT.findById(tokenRT);
+    if (!rt) {
+      const error = new Error("RT tidak ditemukan");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    if (rt.ketuaRT.toString() !== checkWarga._id.toString()) {
+      const error = new Error("Anda bukan ketua RT");
+      error.statusCode = 401;
+      throw error;
+    }
+
+    /* Edit RT Data */
+    const { nomorRT, nomorRW, activeStatus, address, province, city, zipCode } =
+      req.body;
+
+    const newRT = {
+      nomorRT: nomorRT,
+      nomorRW: nomorRW,
+      activeStatus: activeStatus,
+      address: address,
+      province: province,
+      city: city,
+      zipCode: zipCode,
+    };
+
+    const updatedRT = await RT.findByIdAndUpdate(tokenRT, newRT, {
+      new: true,
+    });
+
+    /* Send response */
+    res.status(201).json({
+      message: "RT Anda Berhasil Diubah",
+      data: updatedRT,
+    });
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
 exports.createKetuaRT = async (req, res, next) => {
   try {
     /* Creating validation */
@@ -91,14 +155,8 @@ exports.createKetuaRT = async (req, res, next) => {
     await updatedKetuaRT.save();
 
     const dataRT = await RT.findById(tokenRT)
-      .populate(
-        "ketuaRT",
-        "name email phoneNumber address"
-      )
-      .populate(
-        "member",
-        "name email phoneNumber address"
-      );
+      .populate("ketuaRT", "name email phoneNumber address")
+      .populate("member", "name email phoneNumber address");
 
     res.status(201).json({
       message: "Ketua RT Berhasil Dibuat",
--- apps/rt/routes.js
+++ apps/rt/routes.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { createRT, createKetuaRT } = require("./controller");
+const { createRT, createKetuaRT, editRT } = require("./controller");
 const router = express.Router();
 const { body } = require("express-validator");
 const { authenticateJWT } = require("../auth/controller");
 
 router.post("/rt", createRT);
+router.put("/rt/:tokenRT", authenticateJWT, editRT);
 router.post("/rt/ketua", authenticateJWT, createKetuaRT);
 
 module.exports = router;

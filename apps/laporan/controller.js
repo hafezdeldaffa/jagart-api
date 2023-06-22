d3ea3ec1315148b81c333d00a3e0d4fb677db206
Hafezd El Daffa <hafezdeldaffa9@gmail.com>
2023-06-22 16:25:42 +0700

    Add Delete Laporan endpoint

--- apps/laporan/controller.js
+++ apps/laporan/controller.js
@@ -105,3 +105,107 @@ exports.editLaporan = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.getLaporan = async (req, res, next) => {
+  try {
+    const email = req.user.email;
+
+    const warga = await Warga.findOne({ email: email });
+
+    const laporan = await Laporan.find({
+      tokenRT: warga.tokenRT,
+      tokenWarga: warga._id,
+    });
+
+    res.status(200).json({
+      message: "Data Laporan Berhasil Didapatkan",
+      data: laporan,
+    });
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.getLaporanById = async (req, res, next) => {
+  try {
+    const email = req.user.email;
+
+    const { id } = req.params;
+
+    const warga = await Warga.findOne({ email: email });
+
+    const laporan = await Laporan.findOne({
+      _id: id,
+      tokenRT: warga.tokenRT,
+    });
+
+    res.status(200).json({
+      message: `Data Laporan dengan id: ${id} Berhasil Didapatkan`,
+      data: laporan,
+    });
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.getLaporanByCategory = async (req, res, next) => {
+  try {
+    const { category } = req.query;
+
+    /* Get data from jwt */
+    const email = req.user.email;
+
+    /* Find Warga */
+    const warga = await Warga.find({ email: email }).select("tokenRT");
+
+    /* Find Keuangan */
+    const laporan = await Laporan.find({
+      tokenRT: warga[0].tokenRT,
+      category: { $regex: category, $options: "i" },
+    });
+
+    /* Send response */
+    res.status(200).json({
+      message: `Data Keuangan ${category} Berhasil Ditemukan`,
+      data: laporan,
+    });
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
+
+exports.deleteLaporan = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const { email } = req.user;
+
+    const warga = await Warga.findOne({ email: email });
+
+    const laporan = await Laporan.findById(id);
+
+    if (laporan.tokenWarga !== warga._id) {
+      errorResponse(
+        401,
+        "Anda tidak memiliki akses untuk menghapus data ini",
+        res
+      );
+    }
+
+    await Laporan.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: `Data Laporan dengan id: ${id} Berhasil Dihapus`,
+    });
+  } catch (error) {
+    /* Handling Errors */
+    errorHandling(error);
+    next(error);
+  }
+};
--- apps/laporan/routes.js
+++ apps/laporan/routes.js
@@ -7,12 +7,14 @@ const {
   getLaporan,
   deleteLaporan,
   getLaporanByCategory,
+  getLaporanById,
 } = require("./controller");
 
 routes.post("/laporan", authenticateJWT, createLaporan);
 routes.put("/laporan/:id", authenticateJWT, editLaporan);
-// routes.delete("/laporan/:id", authenticateJWT, deleteLaporan);
-// routes.get("/laporan", authenticateJWT, getLaporan);
-// routes.get("/laporan/category", authenticateJWT, getLaporanByCategory);
+routes.delete("/laporan/id/:id", authenticateJWT, deleteLaporan);
+routes.get("/laporan", authenticateJWT, getLaporan);
+routes.get("/laporan/id/:id", authenticateJWT, getLaporanById);
+routes.get("/laporan/category", authenticateJWT, getLaporanByCategory);
 
 module.exports = routes;
